0a93ff60761bcf87122a3ec4bed5dc69fc226ead
Sascha Willems <webmaster@saschawillems.de>
2024-08-11 10:26:37 +0200

    Added a simple actor maanger

---
+++ base/utilities/ActorManager.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2024 by Sascha Willems - www.saschawillems.de
+ *
+ * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+ */
+
+#include "ActorManager.h"
+
+Actor* ActorManager::addActor(const std::string name, Actor* actor) {
+	actors[name] = actor;
+	return actor;
+}
+
+ActorManager::~ActorManager() {
+	for (auto& it : actors) {
+		delete it.second;
+	}
+}
+
+void Actor::rotate(const glm::vec3 delta)
+{
+	this->rotation += delta;
+}
+
+void Actor::move(const glm::vec3 dir, float deltaT)
+{
+	glm::vec3 camFront;
+	camFront.x = -cos(glm::radians(rotation.x)) * sin(glm::radians(rotation.y));
+	camFront.y = sin(glm::radians(rotation.x));
+	camFront.z = cos(glm::radians(rotation.x)) * cos(glm::radians(rotation.y));
+	camFront = glm::normalize(camFront);
+
+	const float moveSpeed = 0.005f;
+
+	if (dir.z < 0.0f) {
+		position += camFront * moveSpeed;
+	}
+	if (dir.z > 0.0f) {
+		position -= camFront * moveSpeed;
+	}
+	//if (keys.left)
+	//	position -= glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f))) * moveSpeed;
+	//if (keys.right)
+	//	position += glm::normalize(glm::cross(camFront, glm::vec3(0.0f, 1.0f, 0.0f))) * moveSpeed;
+}
+
+glm::mat4 Actor::getMatrix() const
+{
+	const glm::mat4 t = glm::translate(glm::mat4(1.0f), position);
+	glm::mat4 r = glm::rotate(glm::mat4(1.0f), glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
+	r = glm::rotate(r, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
+	r = glm::rotate(r, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
+	const glm::mat4 s = glm::scale(glm::mat4(1.0f), scale);
+	return t * r * s;
+}
---
+++ base/utilities/ActorManager.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 by Sascha Willems - www.saschawillems.de
+ *
+ * This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+ */
+
+#pragma once
+
+#include <string>
+#include "glm/glm.hpp"
+#include "glTF.h"
+
+struct ActorCreateInfo {
+	glm::vec3 position{};
+	glm::vec3 rotation{};
+	glm::vec3 scale{};
+	vkglTF::Model* model{ nullptr };
+	std::string tag{ "" };
+};
+
+class Actor {
+private:
+public:
+	glm::vec3 position{};
+	glm::vec3 rotation{};
+	glm::vec3 scale{};
+	vkglTF::Model* model{ nullptr };
+	std::string tag{ "" };
+	Actor(ActorCreateInfo createInfo) : position(createInfo.position), rotation(createInfo.rotation), scale(createInfo.scale), model(createInfo.model), tag(createInfo.tag) { };
+	void rotate(const glm::vec3 delta);
+	void move(const glm::vec3 dir, float deltaT);
+	glm::mat4 getMatrix() const;
+};
+
+class ActorManager {
+public:
+	std::unordered_map<std::string, Actor*> actors;
+	Actor* addActor(const std::string name, Actor* actor);
+	~ActorManager();
+};
